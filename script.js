42458fbca9e55b5a3a62ed8b8b35058336c61aa8
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-03 20:09:32 -0700

    Write pseudo-code to plan and solve assignment

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log('Hello World')//Test to make sure script is connected to index.html properly
 
+//Pseudo-code
+/*
+FUNCTION getComputerChoice
+    SET randomNumber TO random Integer between 1 and 3
+
+    CASE OF randomNumber
+        1: RETURN 'rock'
+        2: RETURN 'scissors'
+        3: RETURN 'paper'
+        OTHERWISE: RETURN 'There is an error here'
+    END CASE
+END FUNCTION
+*/
+
+
