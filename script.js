27909d90e1c638016e12da5490b5b189e55b700a
Demi Sokoya <68524557+demi-sokoya@users.noreply.github.com>
2025-11-03 22:32:46 -0700

    Flesh out playGame() function
    Move declarations of scores inside playGame()
    Move assignments of humanSelection and computerSelection within function

--- script.js
+++ script.js
@@ -13,27 +13,25 @@ FUNCTION getComputerChoice
     END CASE
 END FUNCTION
 */
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice() {
-    var randomNumber = Math.floor(Math.random() * 3) +  1
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    switch (randomNumber) {
-        case 1: 
-            return 'rock';
+  switch (randomNumber) {
+    case 1:
+      return "rock";
 
-        case 2:
-            return 'paper';
+    case 2:
+      return "paper";
 
-        case 3:
-            return 'scissors';
-    
-        default:
-            console.log("Error in number generation");
-            break;
-    }
+    case 3:
+      return "scissors";
 
+    default:
+      console.log("Error in number generation");
+      break;
+  }
 }
 
 //console.log(getComputerChoice())
@@ -51,14 +49,34 @@ DISPLAY getHumanChoice()
 */
 
 function getHumanChoice() {
-    var userInput = prompt("Rock, Paper, or Scissors?")
-    return userInput;
+  var userInput = prompt("Rock, Paper, or Scissors?");
+  return userInput;
 }
 
 //console.log(getHumanChoice())
 
-//Pseudo-code for playRound
+
+
+//Pseudo-code for playGame
 /*
+FUNCTION playGame()
+    FOR i EQUALS 1, i <= 5, i++
+        playRound(humanSelection, computerSelection)
+    END FOR
+
+    IF humanscore = computerScore THEN
+        DISPLAY "It's... a draw?"
+    ELSE IF humanScore > computerScore THEN
+        DISPLAY "You Win yayyyyyyyyyy!"
+    ELSE 
+        DISPLAY "You lose Boooooooo!"
+
+    
+*/
+
+function playGame() {
+  //Pseudo-code for playRound
+  /*
 FUNCTION playRound(humanChoice, computerChoice) 
     SET humanChoice TO LOWERCASE(humanChoice)
 
@@ -81,60 +99,43 @@ SET computerSelection TO getComputerChoice()
 CALL playRound(humanSelection, computerSelection)
     
  */
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase()
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
 
     if (humanChoice === computerChoice) {
-        console.log("Ooooh, a tie")
+      console.log("Ooooh, a tie");
     } else if (
-        (humanChoice === "paper" && computerChoice === "rock") || 
-        (humanChoice === "rock" && computerChoice === "scissors") || 
-        (humanChoice === "scissors" && computerChoice === "paper")
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log("Yipee, You Win!")
-        humanScore += 1
+      console.log("Yipee, You Win!");
+      humanScore += 1;
     } else {
-        console.log("Shucks, You Lost :( ")
-        computerScore += 1
+      console.log("Shucks, You Lost :( ");
+      computerScore += 1;
     }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-//Pseudo-code for playGame
-/*
-FUNCTION playGame()
-    FOR i EQUALS 1, i <= 5, i++
-        playRound(humanSelection, computerSelection)
-    END FOR
-
-    IF humanscore = computerScore THEN
-        DISPLAY "It's... a draw?"
-    ELSE IF humanScore > computerScore THEN
-        DISPLAY "You Win yayyyyyyyyyy!"
-    ELSE 
-        DISPLAY "You lose Boooooooo!"
-
-    
-*/
-
-function playGame() {
+  }
 
     
 
-    for (i = 1; i <= 5; i++) {
-        playRound(humanSelection, computerSelection)
-    }
+  for (i = 1; i <= 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    
+  }
 
-    if (humanScore == computerScore) {
-        console.log("It's... a draw")
-    } else if (humanScore > computerScore) {
-        console.log("Yayyyy, You Win!")
-    }  else {
-        console.log("You Lose, Boooooooo!")
-    } 
+  if (humanScore == computerScore) {
+    console.log("It's... a draw");
+  } else if (humanScore > computerScore) {
+    console.log("Yayyyy, You Win!");
+  } else {
+    console.log("You Lose, Boooooooo!");
+  }
 }
+
+playGame()
